a61cc206c42adda177eea0b1b5c74f7fc832b06c
Bruno Moyano <108307019+BEMP1@users.noreply.github.com>
2022-09-27 19:18:18 -0300

    Add files via upload

--- ejer2guia10/src/ejer2guia10/Ejer2guia10.java
+++ ejer2guia10/src/ejer2guia10/Ejer2guia10.java
@@ -1,15 +1,20 @@
 package ejer2guia10;
 
+import clase.Lavadora;
 import clase.Televisor;
 
 
 public class Ejer2guia10 {
 
     public static void main(String[] args) {
-        Televisor lava1 = new Televisor();
-        lava1.crearTelevisor(lava1);
+        Lavadora lava1 = new Lavadora();
+        lava1.crearLavadora(lava1);
         lava1.precioFinal(lava1);
         System.out.println(lava1.getPrecio());
+        Televisor tele1 = new Televisor();
+        tele1.crearTelevisor(tele1);
+        tele1.precioFinal(tele1);
+        System.out.println(tele1.getPrecio());
     }
 
 }
